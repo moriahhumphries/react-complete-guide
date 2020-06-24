37ce86099cd152f2414f8d2e5d2732b40945a34b
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-24 18:15:37 -0500

    added button and switch name handler

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+    state = {
+        persons: [
+            { name: "Moriah", age: 32},
+            { name: "Andrew", age: 35},
+            { name: "Kerrigan", age: 9}
+        ]
+    }
+
+    switchNameHandler = () => {
+        console.log("Was clicked!");
+    }
+
   render() {
     return (
       <div className="App">
        <h1>Hi, I'm a React App</h1>
-        <Person name="Moriah" age="32"/>
-          <Person name="Andrew" age="35"/>
-          <Person name="Kerrigan" age="9">My hobbies: screaming</Person>
+          <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>My hobbies: screaming</Person>
       </div>
     );
   }
