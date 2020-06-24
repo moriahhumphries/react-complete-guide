a364760ead5db06510fdddb74fe13a708780500d
Moriah Humphries <moriah.humphries@gmail.com>
2020-06-24 17:35:27 -0500

    updated fields to be dynamic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
        <h1>Hi, I'm a React App</h1>
         <Person name="Moriah" age="32"/>
           <Person name="Andrew" age="35"/>
-          <Person name="Andrew" age="35"/>
+          <Person name="Kerrigan" age="9">My hobbies: screaming</Person>
       </div>
     );
   }
